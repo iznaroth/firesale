ed45494b107649304a24995909b22d96aac068ab
Michael Boschwitz <56314251+bosch137@users.noreply.github.com>
2023-11-18 05:05:27 -0600

    deleted files

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject faceImage;
+    public GameObject handImage;
+
+    public float handSpeed = 1;
+    public float faceLimit = 10;
+
+    public bool startupDone = false;
+    public string nextScene = "";
+    public bool playingStartup;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startupDone = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playingStartup) { SetHandPosition(); }
+        if (startupDone)
+        {
+            startupDone = false;
+            SwitchScenes();
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        this.GetComponentInChildren<Animation>().Play();
+    }
+
+    public void SetHandPosition()
+    {
+        Vector3 goalVector = (Input.mousePosition + faceImage.transform.position) / 2;
+        handImage.transform.position = Vector3.Lerp(handImage.transform.position, goalVector, handSpeed * Time.deltaTime);
+        handImage.transform.right = Input.mousePosition - faceImage.transform.position;
+    }
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadSceneAsync(nextScene);
+    }
+}
